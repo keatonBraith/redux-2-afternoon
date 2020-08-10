25d5bacd06f6d90e081aed6d30da3f07342ab06b
keatonBraith <keeters33@live.com>
2020-08-10 15:17:41 -0600

    completed mission

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,36 +1,59 @@
-import React, { Component } from 'react';
-import Background from './../shared/Background/Background'
-import Chart1 from './../shared/Chart1';
-import Chart2 from './../shared/Chart2';
-import AddPurchase from './../shared/AddPurchase';
-import DisplayPurchases from './../shared/DisplayPurchases';
-import Loading from './../shared/Loading/Loading';
-import Nav from './../shared/Nav';
-import './Budget.css';
-
+import React, { Component } from "react";
+import Background from "./../shared/Background/Background";
+import Chart1 from "./../shared/Chart1";
+import Chart2 from "./../shared/Chart2";
+import AddPurchase from "./../shared/AddPurchase";
+import DisplayPurchases from "./../shared/DisplayPurchases";
+import Loading from "./../shared/Loading/Loading";
+import Nav from "./../shared/Nav";
+import { connect } from "react-redux";
+import "./Budget.css";
+import { requestUserData } from "./../../ducks/userReducer";
+import { requestBudgetData } from "./../../ducks/budgetReducer";
+import { addPurchase } from "./../../ducks/budgetReducer";
+import { removePurchase } from "./../../ducks/budgetReducer";
 
 class Budget extends Component {
+  componentDidMount() {
+    this.props.requestUserData();
+    this.props.requestBudgetData();
+    this.props.addPurchase();
+    this.props.removePurchase();
+  }
 
   render() {
+    const { loading } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
+    const { purchases, budgetLimit } = this.props.budget;
     return (
       <Background>
-        {true ? <Loading /> : null}
-        <div className='budget-container'>
-          <Nav />
-          <div className='content-container'>
+        {loading ? <Loading /> : null}
+        <div className="budget-container">
+          <Nav firstName={firstName} lastName={lastName} />
+          <div className="content-container">
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase} />
+              <DisplayPurchases purchases={purchases} removePurchase={this.props.removePurchase} />
             </div>
-            <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+            <div className="chart-container">
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
       </Background>
-    )
+    );
   }
 }
 
-export default Budget;
+function mapStateToProps(state) {
+  return {
+    budget: state.budget,
+    user: state.user,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  { requestUserData, requestBudgetData, addPurchase, removePurchase }
+)(Budget);
